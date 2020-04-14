304a50caea52babb990d295d14d054ccb768e12d
Eugene Balashov <e.balashov@parity.digital>
2020-04-14 14:52:57 +0300

    Updates

--- precache-manifest.b585b27161a5ee9b0ae561e8e42f338a.js
+++ precache-manifest.b585b27161a5ee9b0ae561e8e42f338a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1db7ffd0e41ab572f49637c2e2a5b63b",
+    "revision": "90a71f9b91f2e28d577885ce9e897230",
     "url": "/rtc1/index.html"
   },
   {
-    "revision": "27ea57b0b216900a73e4",
+    "revision": "070652a38aa5fb8a87d6",
     "url": "/rtc1/static/css/main.f6de8824.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rtc1/static/js/2.037b95e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "27ea57b0b216900a73e4",
-    "url": "/rtc1/static/js/main.7372e11c.chunk.js"
+    "revision": "070652a38aa5fb8a87d6",
+    "url": "/rtc1/static/js/main.b893ef73.chunk.js"
   },
   {
     "revision": "085a54b89f2b2fc65067",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rtc1/precache-manifest.7f7b7dabcc38b0c786915cb932c304a8.js"
+  "/rtc1/precache-manifest.b585b27161a5ee9b0ae561e8e42f338a.js"
 );
 
 self.addEventListener('message', (event) => {
