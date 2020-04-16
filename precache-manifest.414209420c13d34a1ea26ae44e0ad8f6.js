8dacae1c7f456c00223077462894f6ffd8d89707
Eugene Balashov <e.balashov@parity.digital>
2020-04-16 15:05:06 +0300

    Updates

--- precache-manifest.414209420c13d34a1ea26ae44e0ad8f6.js
+++ precache-manifest.414209420c13d34a1ea26ae44e0ad8f6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65371c26c4f2f53e952beeaefb91bc2c",
+    "revision": "7c7dcda82df9aca5bee9649243aa78ac",
     "url": "/rtc1/index.html"
   },
   {
-    "revision": "55b9aea0c97a28f27219",
-    "url": "/rtc1/static/css/main.fa62c434.chunk.css"
+    "revision": "48f4b7755ee191f79d40",
+    "url": "/rtc1/static/css/main.78ce490d.chunk.css"
   },
   {
     "revision": "3a26266f067a9b573265",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rtc1/static/js/2.b4c67b3c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "55b9aea0c97a28f27219",
-    "url": "/rtc1/static/js/main.7e94beb1.chunk.js"
+    "revision": "48f4b7755ee191f79d40",
+    "url": "/rtc1/static/js/main.6c1ec2d0.chunk.js"
   },
   {
     "revision": "085a54b89f2b2fc65067",
